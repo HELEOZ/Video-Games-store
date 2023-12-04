1b5a4661752146c44d5468347822f05389de9b66
The-Original-xD <donovan.xd123@gmail.com>
2023-12-04 08:15:00 -0600

    Update index.js
    Carro de compras actualizado con varias funciones

--- Client/src/components/Cart/index.js
+++ Client/src/components/Cart/index.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import product1 from "../../img/product-1.png";
-import product2 from "../../img/product-3.png";
+import product2 from "../../img/product-3.png"; // Cambiar la ruta si es necesario
 
 import "./style.css";
 
 function Cart(props) {
-  const onClick = (e) => {
-    e.preventDefault();
+  const [cartItems, setCartItems] = useState([
+    // Incluye los elementos de tu carrito aquí
+    {
+      id: 1,
+      name: "Gafas de Sol",
+      price: 20,
+      quantity: 1,
+    },
+    {
+      id: 2,
+      name: "Chanclas",
+      price: 15,
+      quantity: 1,
+    },
+    {
+      id: 3,
+      name: "Producto 2",
+      price: 25,
+      quantity: 1,
+    },
+  ]);
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  const removeItem = (id) => {
+    const updatedCart = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedCart);
+  };
+
+  const updateQuantity = (id, newQuantity) => {
+    if (newQuantity >= 0) {
+      const updatedCart = cartItems.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            quantity: newQuantity,
+          };
+        }
+        return item;
+      });
+      setCartItems(updatedCart);
+    }
+  };
+
+  const updateCart = () => {
+    // Realiza cualquier acción de actualización del carrito que necesites aquí
   };
 
   return (
@@ -19,120 +65,85 @@ function Cart(props) {
           <Row>
             <Col lg={8}>
               <div className="cart-table-left">
-                <h3>Shopping Cart</h3>
+                <h3>Carro de Compras</h3>
                 <div className="table-responsive cart_box">
                   <table className="table">
                     <thead>
                       <tr>
-                        <th>Preview</th>
-                        <th>Product</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
+                        <th>Imagen</th>
+                        <th>Producto</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
                         <th>Total</th>
-                        <th>Action</th>
+                        <th>Acción</th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr className="shop-cart-item">
-                        <td className="fag-cart-preview">
-                          <Link to="/product-single">
-                            <img src={product1} alt="cart-1" />
-                          </Link>
-                        </td>
-                        <td className="fag-cart-product">
-                          <Link to="/product-single">
-                            <p>Sunglasses</p>
-                          </Link>
-                        </td>
-                        <td className="fag-cart-price">
-                          <p>$20</p>
-                        </td>
-                        <td className="fag-cart-quantity">
-                          <div className="num-block skin-2">
-                            <div className="num-in">
-                              <input
-                                type="number"
-                                className="in-num"
-                                defaultValue={1}
-                                readOnly=""
-                              />
+                      {cartItems.map((item) => (
+                        <tr key={item.id} className="shop-cart-item">
+                          <td className="fag-cart-preview">
+                            <Link to="/product-single">
+                              {item.id === 1 && <img src={product1} alt={item.name} />}
+                              {item.id === 2 && <img src={product2} alt={item.name} />}
+                              {item.id === 3 && <img src={product2} alt={item.name} />}
+                            </Link>
+                          </td>
+                          <td className="fag-cart-product">
+                            <Link to="/product-single">
+                              <p>{item.name}</p>
+                            </Link>
+                          </td>
+                          <td className="fag-cart-price">
+                            <p>${item.price}</p>
+                          </td>
+                          <td className="fag-cart-quantity">
+                            <div className="num-block skin-2">
+                              <div className="num-in">
+                                <input
+                                  type="number"
+                                  className="in-num"
+                                  value={item.quantity}
+                                  onChange={(e) => updateQuantity(item.id, parseInt(e.target.value))}
+                                />
+                              </div>
                             </div>
-                          </div>
-                        </td>
-                        <td className="fag-cart-total">
-                          <p>$20</p>
-                        </td>
-                        <td className="fag-cart-close">
-                          <Link to="/" onClick={onClick}>
-                            <FaTimes />
-                          </Link>
-                        </td>
-                      </tr>
-                      <tr className="shop-cart-item">
-                        <td className="fag-cart-preview">
-                          <Link to="/product-single">
-                            <img src={product2} alt="cart-1" />
-                          </Link>
-                        </td>
-                        <td className="fag-cart-product">
-                          <Link to="/product-single">
-                            <p>Flip-flops</p>
-                          </Link>
-                        </td>
-                        <td className="fag-cart-price">
-                          <p>$15</p>
-                        </td>
-                        <td className="fag-cart-quantity">
-                          <div className="num-block skin-2">
-                            <div className="num-in">
-                              <input
-                                type="number"
-                                className="in-num"
-                                defaultValue={1}
-                                readOnly=""
-                              />
-                            </div>
-                          </div>
-                        </td>
-                        <td className="fag-cart-total">
-                          <p>$15</p>
-                        </td>
-                        <td className="fag-cart-close">
-                          <Link to="/" onClick={onClick}>
-                            <FaTimes />
-                          </Link>
-                        </td>
-                      </tr>
+                          </td>
+                          <td className="fag-cart-total">
+                            <p>${item.price * item.quantity}</p>
+                          </td>
+                          <td className="fag-cart-close">
+                            <button onClick={() => removeItem(item.id)}>
+                              <FaTimes />
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
                     </tbody>
                   </table>
                 </div>
                 <div className="cart-clear">
-                  <Link to="/" onClick={onClick}>
-                    Vaciar Carro
-                  </Link>
-                  <Link to="/" onClick={onClick}>
-                    Actualizar Carro
-                  </Link>
+                  <button onClick={clearCart}>Vaciar Carro</button>
+                  <button onClick={updateCart}>Actualizar Carro</button>
                 </div>
               </div>
             </Col>
             <Col lg={4}>
-              <div className="order-summury-box">
-                <h3>Resumen del pedido</h3>
-                <div className="summury-inn">
+              <div className="order-summary-box">
+                <h3>Resumen del Pedido</h3>
+                <div className="summary-inn">
                   <table>
                     <tbody>
                       <tr>
                         <td>Subtotal</td>
-                        <td>$35</td>
+                        <td>${cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}</td>
                       </tr>
                       <tr>
-                        <td>Gastos de Envío </td>
-                        <td>Envío  gratutito</td>
+                        <td>Gastos de Envío</td>
+                        <td>Envío Gratuito</td>
                       </tr>
                       <tr>
-                        <td>Total de la orden</td>
-                        <td>$35</td>
+                        <td>Total de la Orden</td>
+                        <td>${cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}</td>
                       </tr>
                     </tbody>
                   </table>
@@ -140,8 +151,7 @@ function Cart(props) {
               </div>
               <div className="checkout-action">
                 <Link to="/checkout" className="fag-btn">
-                  Proceder a facturar <span />
-                  
+                  Proceder a Facturar <span />
                 </Link>
               </div>
             </Col>
@@ -153,3 +163,4 @@ function Cart(props) {
 }
 
 export default Cart;
+
